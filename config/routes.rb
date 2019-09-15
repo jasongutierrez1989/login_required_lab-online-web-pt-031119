b23f8c336d5a50bf6a43ee3ef23de0215853402c
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-09-15 02:32:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/Controller'
-
-  get 'secrets/Controller'
+  get '/show' => 'secrets#show'
+  post '/create' => 'sessions#create'
+  get '/new' => 'sessions#new'
+  get '/home' => 'sessions#home'
+  post '/destroy' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
